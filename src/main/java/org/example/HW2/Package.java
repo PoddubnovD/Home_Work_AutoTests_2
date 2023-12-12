9ab0b3780f872932f921320841800eb0ef623f04
Dmitry <poddubnovd@yandex.ru>
2023-12-12 21:55:41 +0300

    HW2

---
+++ src/main/java/org/example/HW2/Package.java
@@ -0,0 +1,14 @@
+package org.example.HW2;
+
+public class Package implements Products {
+    @Override
+    public void writeDown() {
+        System.out.println("Упаковка");
+    }
+
+    @Override
+    public Long getSum() {
+
+        return getSum();
+    }
+}
---
+++ src/main/java/org/example/HW2/Products.java
@@ -0,0 +1,6 @@
+package org.example.HW2;
+
+public interface Products {
+    public void writeDown();
+    public Long getSum();
+}
